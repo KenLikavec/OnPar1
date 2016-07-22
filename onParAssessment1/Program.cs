6a2cd7f6345404ebabc916965a40be9ae4160a76
Ken Likavec <kdl17@zips.uakron.edu>
2016-07-21 20:56:40 -0400

    first commit

---
+++ onParAssessment1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onParAssessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Problem 1
+
+            Console.WriteLine("Enter a number to sum.");
+            int a = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[] { a };
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
